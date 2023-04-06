22c2967828c4c76143a8a871a7475dc42490133e
Евгений Лях <eaglet.hoagie_0h@icloud.com>
2023-04-06 18:41:41 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack func."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон'
                 f' противнику равный {5 + randint(3, 5)}')
@@ -14,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defence func."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Special Attack func."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное'
                 f' умение «Выносливость {80 + 25}»')
@@ -35,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Start training func."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный'
               f' боец ближнего боя.')
@@ -62,6 +67,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choice char func."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -83,7 +89,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -93,6 +100,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
